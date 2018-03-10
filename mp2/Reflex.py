9073f2052d72c9434fb7145c6c2197e6274d26b2
YaboLee <yaboli2@illinois.edu>
2018-03-09 21:29:40 -0600

    delete unnecessary

--- mp2/Reflex.py
+++
@@ -1,9 +0,0 @@
-import Board
-
-# Return next move for reflex agent;
-def evaluate(board, symbol):
-    all_pos = []
-    for s in board.board:
-        if board.board[s] == symbol:
-            all_pos.append(s)
-    all_pos = sorted(all_pos)
