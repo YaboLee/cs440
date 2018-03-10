af4b956a952cfd6667f1aef3b5249e3ae940ccee
YaboLee <yaboli2@illinois.edu>
2018-03-09 20:53:13 -0600

    part2 frustructure

---
+++ mp2/Reflex.py
@@ -0,0 +1,9 @@
+import Board
+
+# Return next move for reflex agent;
+def evaluate(board, symbol):
+    all_pos = []
+    for s in board.board:
+        if board.board[s] == symbol:
+            all_pos.append(s)
+    all_pos = sorted(all_pos)
